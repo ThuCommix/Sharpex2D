f001e3ea0131261d56c99669c1b042e73fa21da5
ThuCommix <admin@thucommix.de>
2013-09-25 12:32:59 +0200

    implement active waiting & remove unused files
    ActiveWaiting do not work properly yet, need to get further patches.

---
+++ Sharpex.GameLibrary/Framework/Game/Timing/IActiveWaiting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpexGL.Framework.Game.Timing
+{
+    internal interface IActiveWaiting
+    {
+        /// <summary>
+        /// Waits the thread for the specified miliseconds.
+        /// </summary>
+        /// <param name="miliseconds">The Miliseconds.</param>
+        void Busy(TimeSpan miliseconds);
+    }
+}
