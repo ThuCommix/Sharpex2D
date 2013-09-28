61a0005e51ec7fc15a61e8bd59bf650989cd50f1
ThuCommix <admin@thucommix.de>
2013-09-28 16:00:24 +0200

    SharpexGL.DirectSound 0.5.0.0

---
+++ SharpexGL.DirectSound/CSCoreSoundInitializer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SharpexGL.Framework.Media.Sound.DiectSound
+{
+    public class CSCoreSoundInitializer : ISoundInitializer
+    {
+        public ISoundProvider CreateProvider()
+        {
+            return new CSCoreSoundProvider();
+        }
+    }
+}
