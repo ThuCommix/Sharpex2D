cfa427673f5665cade39d547c39121d645c65a05
ThuCommix <admin@thucommix.de>
2013-10-16 22:14:59 +0200

    Removed redundant this qualifier

--- Sharpex.GameLibrary/Framework/Common/Randomization/GameRandom.cs
+++ Sharpex.GameLibrary/Framework/Common/Randomization/GameRandom.cs
@@ -4,13 +4,13 @@ namespace SharpexGL.Framework.Common.Randomization
 {
     public class GameRandom
     {
-        private Random _random;
+        private readonly Random _random;
         /// <summary>
         /// Initializes a new GameRandom.
         /// </summary>
         public GameRandom()
         {
-            this._random = new Random();
+            _random = new Random();
         }
         /// <summary>
         /// Returns Int32 between Int32 min - and max value.
@@ -18,7 +18,7 @@ namespace SharpexGL.Framework.Common.Randomization
         /// <returns>Int</returns>
         public int Next()
         {
-            return this._random.Next(-2147483648, 2147483647);
+            return _random.Next(-2147483648, 2147483647);
         }
         /// <summary>
         /// Returns Int32 between 0 and max.
@@ -27,7 +27,7 @@ namespace SharpexGL.Framework.Common.Randomization
         /// <returns>Int</returns>
         public int Next(int max)
         {
-            return this._random.Next(0, max);
+            return _random.Next(0, max);
         }
         /// <summary>
         /// Returns Int32 between min and max.
@@ -37,7 +37,7 @@ namespace SharpexGL.Framework.Common.Randomization
         /// <returns>Int</returns>
         public int Next(int min, int max)
         {
-            return this._random.Next(min, max);
+            return _random.Next(min, max);
         }
         /// <summary>
         /// Returns a float between 0 and 1.
@@ -45,7 +45,7 @@ namespace SharpexGL.Framework.Common.Randomization
         /// <returns>Float</returns>
         public float NextFloat()
         {
-            return (float)this._random.NextDouble();
+            return (float)_random.NextDouble();
         }
         /// <summary>
         /// Returns a double between 0 and 1.
@@ -53,7 +53,7 @@ namespace SharpexGL.Framework.Common.Randomization
         /// <returns>Double</returns>
         public double NextDouble()
         {
-            return this._random.NextDouble();
+            return _random.NextDouble();
         }
         /// <summary>
         /// Returns true or false.
@@ -61,7 +61,7 @@ namespace SharpexGL.Framework.Common.Randomization
         /// <returns>Boolean</returns>
         public bool NextBoolean()
         {
-            return this.NextBoolean(0.5);
+            return NextBoolean(0.5);
         }
         /// <summary>
         /// Returns true or false based on the probability.
@@ -70,7 +70,7 @@ namespace SharpexGL.Framework.Common.Randomization
         /// <returns>Boolean</returns>
         public bool NextBoolean(double probability)
         {
-            return this._random.NextDouble() <= probability;
+            return _random.NextDouble() <= probability;
         }
     }
 }
