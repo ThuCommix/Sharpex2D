24298509377770f12a2174da034eb185cdae221a
ThuCommix <admin@thucommix.de>
2013-10-17 11:53:35 +0200

    Updatet ThreadInvoker
    Renamed MainThreadInvoker to ThreadInvoker and made some optimations

---
+++ Sharpex.GameLibrary/Framework/Common/Threads/ThreadInvoker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SharpexGL.Framework.Common.Threads
+{
+    public class ThreadInvoker : IThreadInvoker
+    {
+        /// <summary>
+        /// Initializes a new ThreadInvoker class.
+        /// </summary>
+        public ThreadInvoker()
+        {
+            _invokeableControl = new Control();
+        }
+
+        private readonly Control _invokeableControl;
+
+        /// <summary>
+        /// Invokes an action in a special thread.
+        /// </summary>
+        /// <param name="action">The Action.</param>
+        public void Invoke(Action action)
+        {
+            //There is no other known way, to invoke in the main thread.
+
+            if (!_invokeableControl.Created || !_invokeableControl.IsHandleCreated)
+            {
+                _invokeableControl.CreateControl();
+                while (!_invokeableControl.IsHandleCreated) {}
+            }
+
+            _invokeableControl.Invoke(action);
+        }
+    }
+}
