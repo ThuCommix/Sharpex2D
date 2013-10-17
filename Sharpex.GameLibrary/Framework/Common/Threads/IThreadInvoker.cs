81b7c6e4680009849ceca06963b7e9925ba76aa4
ThuCommix <admin@thucommix.de>
2013-10-17 08:09:06 +0200

    Added ThreadInvoker
    Possibility of invoking action into multiple threads

---
+++ Sharpex.GameLibrary/Framework/Common/Threads/IThreadInvoker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SharpexGL.Framework.Common.Threads
+{
+    public interface IThreadInvoker
+    {
+        /// <summary>
+        /// Invokes an action in a special thread.
+        /// </summary>
+        /// <param name="action">The Action.</param>
+        void Invoke(Action action);
+    }
+}
