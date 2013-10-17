40faa6defd117be1d2123682b8819b90178b2967
ThuCommix <admin@thucommix.de>
2013-10-17 08:09:31 +0200

    Added MainThreadInvoker
    Invokes in the main thread.

---
+++ Sharpex.GameLibrary/Framework/Common/Threads/MainThreadInvoker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SharpexGL.Framework.Common.Threads
+{
+    public class MainThreadInvoker : IThreadInvoker
+    {
+        /// <summary>
+        /// Invokes an action in a special thread.
+        /// </summary>
+        /// <param name="action">The Action.</param>
+        public void Invoke(Action action)
+        {
+            //There is no other known way, to invoke in the main thread.
+            var control = new Control();
+            MethodInvoker br = action.Invoke;
+            control.Invoke(br);
+        }
+    }
+}
