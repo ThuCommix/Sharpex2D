22307d463f86e7101300b89109a7500657729fae
ThuCommix <admin@thucommix.de>
2013-10-24 22:45:25 +0200

    Added CircleOriginType

---
+++ Sharpex.GameLibrary/Framework/Media/Sound/OriginTypes/CircleOriginType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SharpexGL.Framework.Media.Sound.OriginTypes
+{
+    public class CircleOriginType : IOriginType
+    {
+        #region IOriginType Implementation
+
+        /// <summary>
+        /// Gets the Identifer.
+        /// </summary>
+        public Guid Guid { get; private set; }
+
+        #endregion
+
+        public CircleOriginType()
+        {
+            Guid = new Guid("74F64914-A3E5-4A1F-95F3-C8CEAC8E8F6A");
+            Radius = 10;
+        }
+
+        /// <summary>
+        /// Sets or gets the Circle Radius.
+        /// </summary>
+        public float Radius { set; get; }
+    }
+}
