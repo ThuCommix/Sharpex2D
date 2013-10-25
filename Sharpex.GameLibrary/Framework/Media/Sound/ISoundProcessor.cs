d8afd0540bccc96ce3dce7223d76115c3de6bdb6
ThuCommix <admin@thucommix.de>
2013-10-25 13:13:17 +0200

    Added possibility to add SoundProcessors
    This is a replacement for origin sound, but more effectiv and extensible

---
+++ Sharpex.GameLibrary/Framework/Media/Sound/ISoundProcessor.cs
@@ -0,0 +1,18 @@
+﻿using SharpexGL.Framework.Math;
+
+namespace SharpexGL.Framework.Media.Sound
+{
+    public interface ISoundProcessor
+    {
+        /// <summary>
+        /// Updates the SoundProcessor.
+        /// </summary>
+        /// <param name="listenerPosition">The ListenerPosition.</param>
+        /// <param name="soundOriginPosition">The SoundOriginPosition.</param>
+        void Update(Vector2 listenerPosition, Vector2 soundOriginPosition);
+        /// <summary>
+        /// Gets the SoundManager.
+        /// </summary>
+        SoundManager SoundManager { get; }
+    }
+}
