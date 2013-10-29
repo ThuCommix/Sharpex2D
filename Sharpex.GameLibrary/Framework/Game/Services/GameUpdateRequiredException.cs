d795c4afd9606a211f36ff498c661ff04ef0a46b
ThuCommix <admin@thucommix.de>
2013-10-29 07:21:31 +0100

    Added an Exception for game is not updated

---
+++ Sharpex.GameLibrary/Framework/Game/Services/GameUpdateRequiredException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SharpexGL.Framework.Game.Services
+{
+    public class GameUpdateRequiredException : Exception
+    {
+        /// <summary>
+        /// Initializes a new GameUpdateRequiredException class.
+        /// </summary>
+        public GameUpdateRequiredException()
+        {
+            
+        }
+        /// <summary>
+        /// Initializes a new GameUpdateRequiredException class.
+        /// </summary>
+        /// <param name="message">The Message.</param>
+        public GameUpdateRequiredException(string message)
+        {
+            _message = message;
+        }
+
+        private readonly string _message = "";
+
+        public override string Message
+        {
+            get
+            {
+                return _message;
+            }
+        }
+    }
+}
