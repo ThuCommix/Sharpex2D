293755c814bb8048027b4f80ca68f670539d3851
ThuCommix <admin@thucommix.de>
2013-10-29 07:20:08 +0100

    Added NetworkService

---
+++ Sharpex.GameLibrary/Framework/Game/Services/NetworkNotAvailableException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpexGL.Framework.Game.Services
+{
+    public class NetworkNotAvailableException : Exception
+    {
+        /// <summary>
+        /// Initializes a new NetworkNotAvailableException class.
+        /// </summary>
+        public NetworkNotAvailableException()
+        {
+            
+        }
+        /// <summary>
+        /// Initializes a new NetworkNotAvailableException class.
+        /// </summary>
+        /// <param name="message">The Message.</param>
+        public NetworkNotAvailableException(string message)
+        {
+            _message = message;
+        }
+
+        private readonly string _message = "";
+
+        public override string Message
+        {
+            get
+            {
+                return _message;
+            }
+        }
+    }
+}
---
+++ Sharpex.GameLibrary/Framework/Game/Services/NetworkService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.NetworkInformation;
+
+namespace SharpexGL.Framework.Game.Services
+{
+    public class NetworkService
+    {
+        /// <summary>
+        /// Checks if the Network is available. Throws an NetworkNotAvailableException if not.
+        /// </summary>
+        public static void CheckAvailability()
+        {
+            var pingRequest = new Ping();
+            var reply = pingRequest.Send("www.google.de");
+            if (reply == null && reply.Status != IPStatus.Success)
+            {
+                throw new NetworkNotAvailableException("The network is not available.");
+            }
+        }
+    }
+}
