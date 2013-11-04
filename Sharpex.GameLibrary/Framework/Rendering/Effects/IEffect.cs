a19f7a696a8cf5dc19bd986a7cf19f674e016f9f
ThuCommix <admin@thucommix.de>
2013-11-04 21:22:14 +0100

    Added Effects for drawing

---
+++ Sharpex.GameLibrary/Framework/Rendering/Effects/IEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SharpexGL.Framework.Rendering.Effects
+{
+    public interface IEffect
+    {
+        /// <summary>
+        /// Gets the Guid-Identifer.
+        /// </summary>
+        Guid Guid { get; }
+        /// <summary>
+        /// Sets or gets the Duration.
+        /// </summary>
+        float Duration { set; get; }
+        /// <summary>
+        /// Starts the Effect.
+        /// </summary>
+        void Start();
+    }
+}
