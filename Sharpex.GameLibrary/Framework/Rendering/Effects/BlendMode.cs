1004c16a79ad3f87a311a58184f67599d22a9c60
ThuCommix <admin@thucommix.de>
2013-11-05 15:04:23 +0100

    Added BlendMode

---
+++ Sharpex.GameLibrary/Framework/Rendering/Effects/BlendMode.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SharpexGL.Framework.Rendering.Effects
+{
+    public enum BlendMode
+    {
+        /// <summary>
+        /// FadeIn Blend effect.
+        /// </summary>
+        FadeIn,
+        /// <summary>
+        /// FadeOut Blend effect.
+        /// </summary>
+        FadeOut
+    }
+}
