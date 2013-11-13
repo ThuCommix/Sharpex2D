5f1fa3d8ebfb31641255d7718c429e77f7cc542d
ThuCommix <admin@thucommix.de>
2013-11-13 16:45:18 +0100

    Added ITimeable interface

---
+++ Sharpex.GameLibrary/Framework/Game/Timing/ITimeable.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SharpexGL.Framework.Game.Timing
+{
+    public interface ITimeable
+    {
+        /// <summary>
+        /// Sets or gets the Intervall.
+        /// </summary>
+        float Intervall { get; set; }
+    }
+}
