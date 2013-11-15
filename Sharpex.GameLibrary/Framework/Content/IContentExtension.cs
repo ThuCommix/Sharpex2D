2c6fd280204ea6c08ce1b5d1787061ded034164a
ThuCommix <admin@thucommix.de>
2013-11-15 16:27:30 +0100

    Added IContentExtension

---
+++ Sharpex.GameLibrary/Framework/Content/IContentExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SharpexGL.Framework.Content
+{
+    public interface IContentExtension
+    {
+        /// <summary>
+        /// Gets the Guid of the ContentExtension.
+        /// </summary>
+        Guid Guid { get; }
+        /// <summary>
+        /// Gets the ContentType.
+        /// </summary>
+        Type ContentType { get; }
+        /// <summary>
+        /// Creates the IContent from the given Path.
+        /// </summary>
+        /// <param name="path">The Path.</param>
+        /// <returns>IContent</returns>
+        IContent Create(string path);
+    }
+}
