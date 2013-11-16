c1d279ad77ee2daa0a7bd5109148d1a0c7623a1f
ThuCommix <admin@thucommix.de>
2013-11-16 16:29:45 +0100

    Added exceptions

---
+++ Sharpex.GameLibrary/Framework/Exceptions/BaseException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SharpexGL.Framework.Exceptions
+{
+    public class BaseException : Exception
+    {
+        /// <summary>
+        /// Initializes a new BaseException class.
+        /// </summary>
+        public BaseException()
+        {
+            
+        }
+        /// <summary>
+        /// Initializes a new BaseException class.
+        /// </summary>
+        /// <param name="message">The Message.</param>
+        public BaseException(string message)
+        {
+            _message = message;
+        }
+        /// <summary>
+        /// Initializes a new BaseException class.
+        /// </summary>
+        /// <param name="message">The Message.</param>
+        /// <param name="innerException">The InnerException</param>
+        public BaseException(string message, BaseException innerException)
+        {
+            _message = message;
+            InnerException = innerException;
+        }
+
+        private readonly string _message = "";
+
+        public override string Message
+        {
+            get
+            {
+                return _message;
+            }
+        }
+        /// <summary>
+        /// Gets the InnerException.
+        /// </summary>
+        public new BaseException InnerException { private set; get; } 
+    }
+}
---
+++ Sharpex.GameLibrary/Framework/Exceptions/SGLNotInitializedException.cs
@@ -0,0 +1,24 @@
+﻿
+namespace SharpexGL.Framework.Exceptions
+{
+    public class SGLNotInitializedException : BaseException
+    {
+        /// <summary>
+        /// Initializes a new SGLNotInitializedException class.
+        /// </summary>
+        /// <param name="message">The Message.</param>
+        public SGLNotInitializedException(string message) : base(message)
+        {
+            
+        }
+        /// <summary>
+        /// Initializes a new SGLNotInitializedException class.
+        /// </summary>
+        /// <param name="message">The Message.</param>
+        /// <param name="innerException">The InnerException.</param>
+        public SGLNotInitializedException(string message, BaseException innerException) : base(message, innerException)
+        {
+            
+        }
+    }
+}
