2951bb2599c0855e7558e19f01e3843a13750ee0
ThuCommix <admin@thucommix.de>
2013-11-16 16:10:19 +0100

    The initializer now requires a window handle

--- Sharpex.GameLibrary/SGLInitializer.cs
+++ Sharpex.GameLibrary/SGLInitializer.cs
@@ -1,4 +1,5 @@
-﻿using SharpexGL.Framework.Game;
+﻿using System;
+using SharpexGL.Framework.Game;
 
 namespace SharpexGL
 {
@@ -18,49 +19,14 @@ namespace SharpexGL
         /// Initialize SGLInitializer.
         /// </summary>
         /// <param name="gameInstance">The Game.</param>
-        public SGLInitializer(Game gameInstance)
+        /// <param name="targetHandle">The SurfaceTargetHandle.</param>
+        public SGLInitializer(Game gameInstance, IntPtr targetHandle)
         {
             Width = 640;
             Height = 480;
             GameInstance = gameInstance;
+            TargetHandle = targetHandle;
             TargetFramesPerSecond = 60;
-        }
-        /// <summary>
-        /// Initialize SGLInitializer.
-        /// </summary>
-        /// <param name="width">The Width.</param>
-        /// <param name="height">The Height.</param>
-        public SGLInitializer(int width, int height)
-        {
-            Width = width;
-            Height = height;
-            TargetFramesPerSecond = 60;
-        }
-        /// <summary>
-        /// Initialize SGLInitializer.
-        /// </summary>
-        /// <param name="width">The Width.</param>
-        /// <param name="height">The Height.</param>
-        /// <param name="framesPerSecond">The TargetFrameRate.</param>
-        public SGLInitializer(int width, int height, int framesPerSecond)
-        {
-            Width = width;
-            Height = height;
-            TargetFramesPerSecond = framesPerSecond;
-        }
-        /// <summary>
-        /// Initialize SGLInitializer.
-        /// </summary>
-        /// <param name="width">The Width.</param>
-        /// <param name="height">The Height.</param>
-        /// <param name="framesPerSecond">The TargetFrameRate.</param>
-        /// <param name="gameInstance">The Game.</param>
-        public SGLInitializer(int width, int height, int framesPerSecond , Game gameInstance)
-        {
-            Width = width;
-            Height = height;
-            GameInstance = gameInstance;
-            TargetFramesPerSecond = framesPerSecond;
         }
         /// <summary>
         /// Gets or sets the Width.
@@ -78,5 +44,9 @@ namespace SharpexGL
         /// Gets or sets the Game.
         /// </summary>
         public Game GameInstance { set; get; }
+        /// <summary>
+        /// Sets or gets the TargetHandle.
+        /// </summary>
+        public IntPtr TargetHandle { get; set; }
     }
 }
