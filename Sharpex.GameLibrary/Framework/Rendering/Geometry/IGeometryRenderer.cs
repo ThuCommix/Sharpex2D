8ad063b84ee466320b083d93a34d9c8c695734ae
ThuCommix <admin@thucommix.de>
2013-12-06 19:31:14 +0100

    Added IPen

--- Sharpex.GameLibrary/Framework/Rendering/Geometry/IGeometryRenderer.cs
+++ Sharpex.GameLibrary/Framework/Rendering/Geometry/IGeometryRenderer.cs
@@ -7,43 +7,43 @@ namespace SharpexGL.Framework.Rendering.Geometry
         /// <summary>
         /// Draws a Rectangle.
         /// </summary>
-        /// <param name="color">The Color.</param>
+        /// <param name="pen">The Pen.</param>
         /// <param name="rectangle">The Rectangle.</param>
-        void DrawRectangle(Color color, Rectangle rectangle);
+        void DrawRectangle(IPen pen, Rectangle rectangle);
         /// <summary>
         /// Draws a Line between two points.
         /// </summary>
-        /// <param name="color">The Color.</param>
+        /// <param name="pen">The Pen.</param>
         /// <param name="start">The Startpoint.</param>
         /// <param name="target">The Targetpoint.</param>
-        void DrawLine(Color color, Vector2 start, Vector2 target);
+        void DrawLine(IPen pen, Vector2 start, Vector2 target);
         /// <summary>
         /// Draws a Ellipse.
         /// </summary>
-        /// <param name="color">The Color.</param>
+        /// <param name="pen">The Pen.</param>
         /// <param name="rectangle">The Rectangle.</param>
-        void DrawEllipse(Color color, Rectangle rectangle);
+        void DrawEllipse(IPen pen, Rectangle rectangle);
         /// <summary>
         /// Draws an Arc.
         /// </summary>
-        /// <param name="color">The Color.</param>
+        /// <param name="pen">The Pen.</param>
         /// <param name="rectangle">The Rectangle.</param>
         /// <param name="startAngle">The StartAngle.</param>
         /// <param name="sweepAngle">The SweepAngle.</param>
-        void DrawArc(Color color, Rectangle rectangle, float startAngle, float sweepAngle);
+        void DrawArc(IPen pen, Rectangle rectangle, float startAngle, float sweepAngle);
         /// <summary>
         /// Draws a Polygon.
         /// </summary>
-        /// <param name="color">The Color.</param>
+        /// <param name="pen">The Pen.</param>
         /// <param name="points">The Points.</param>
-        void DrawPolygon(Color color, Vector2[] points);
+        void DrawPolygon(IPen pen, Vector2[] points);
         /// <summary>
         /// Draws a corner-rounded Rectangle.
         /// </summary>
-        /// <param name="color">The Color.</param>
+        /// <param name="pen">The Pen.</param>
         /// <param name="rectangle">The Rectangle.</param>
         /// <param name="radius">The Radius.</param>
-        void DrawRoundedRectangle(Color color, Rectangle rectangle, int radius);
+        void DrawRoundedRectangle(IPen pen, Rectangle rectangle, int radius);
         /// <summary>
         /// Fills a Rectangle.
         /// </summary>
---
+++ Sharpex.GameLibrary/Framework/Rendering/IPen.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SharpexGL.Framework.Rendering
+{
+    public interface IPen
+    {
+        /// <summary>
+        /// Sets or gets the Size of the Pen.
+        /// </summary>
+        float Width { set; get; }
+        /// <summary>
+        /// Sets or gets the Color of the Pen.
+        /// </summary>
+        Color Color { set; get; }
+    }
+}
