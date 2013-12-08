6a949f6d70fa1558a2077988a6ee35595f380c96
ThuCommix <admin@thucommix.de>
2013-12-08 12:57:51 +0100

    Sharpex.GameLibrary 0.1.619

--- Sharpex.GameLibrary/SGL.cs
+++ Sharpex.GameLibrary/SGL.cs
@@ -42,7 +42,7 @@ namespace SharpexGL
         /// <summary>
         /// Gets the Version of SGL.
         /// </summary>
-        public static string Version { get { return "0.1.549"; } }
+        public static string Version { get { return "0.1.619"; } }
         /// <summary>
         /// Determines, if SGL is initialized.
         /// </summary>
