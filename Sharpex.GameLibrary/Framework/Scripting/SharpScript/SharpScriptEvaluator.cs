4d360d6dab44b2234e837729f680db34048b2852
ThuCommix <admin@thucommix.de>
2014-03-30 16:38:48 +0200

    Added class to store compiled scripts
    This should prevent unnecessary cpu usage if a script runs multiple
    times.

--- Sharpex.GameLibrary/Framework/Scripting/SharpScript/SharpScriptEvaluator.cs
+++ Sharpex.GameLibrary/Framework/Scripting/SharpScript/SharpScriptEvaluator.cs
@@ -1,38 +1,13 @@
-﻿using SharpexGL.Framework.Content;
+﻿using System.Reflection;
+using SharpexGL.Framework.Content;
+using SharpexGL.Framework.Debug.Logging;
 
 namespace SharpexGL.Framework.Scripting.SharpScript
 {
     public class SharpScriptEvaluator : IScriptEvaluator
     {
-        /// <summary>
-        /// Evaluate the script content.
-        /// </summary>
-        /// <param name="script">The Script.</param>
-        /// <param name="objects">The Objects.</param>
-        public void Evaluate(IScript script, params object[] objects)
-        {
-            if (script.GetType() != typeof (SharpScript))
-            {
-                throw new ScriptException("The given script does not match the SharpScript sheme.");
-            }
+        private readonly ScriptStorageBuffer _storageBuffer;
 
-            var sharpScript = script as SharpScript;
-
-            var assembly = ScriptCompiler.CompileToAssembly(sharpScript);
-
-            var fType = assembly.GetTypes()[0];
-            var iType = fType.GetInterface("IScriptEntry");
-
-            if (iType != null)
-            {
-                var scriptbase = (IScriptEntry) assembly.CreateInstance(fType.FullName);
-                if (scriptbase != null) scriptbase.Main(objects);
-            }
-            else
-            {
-                throw new ScriptException("IScriptEntry interface not found.");
-            }
-        }
         /// <summary>
         /// Initializes a new SharpScriptEvaluator.
         /// </summary>
@@ -41,6 +16,61 @@ namespace SharpexGL.Framework.Scripting.SharpScript
             //Register loading technique
 
             SGL.Components.Get<ContentManager>().Extend(new SharpScriptLoader());
+            _storageBuffer = new ScriptStorageBuffer();
+        }
+
+        /// <summary>
+        /// Evaluate the script content.
+        /// </summary>
+        /// <param name="script">The Script.</param>
+        /// <param name="objects">The Objects.</param>
+        public void Evaluate(IScript script, params object[] objects)
+        {
+            if (script.GetType() != typeof(SharpScript))
+            {
+                throw new ScriptException("The given script does not match the SharpScript sheme.");
+            }
+
+            var sharpScript = script as SharpScript;
+
+            //check if the script was compiled previously
+
+            Assembly assembly;
+
+            if (_storageBuffer.Exists(script.Guid))
+            {
+                assembly = _storageBuffer[script.Guid];
+                Log.Next("Used previously compiled script.", LogLevel.Info, LogMode.StandardOut);
+            }
+            else
+            {
+                assembly = ScriptCompiler.CompileToAssembly(sharpScript);
+                if (!_storageBuffer.Exists(script.Guid))
+                {
+                    _storageBuffer.Add(script.Guid, assembly);
+                }
+            }
+
+            var fType = assembly.GetTypes()[0];
+            var iType = fType.GetInterface("IScriptEntry");
+
+            if (iType != null)
+            {
+                var scriptbase = (IScriptEntry)assembly.CreateInstance(fType.FullName);
+                if (scriptbase != null)
+                {
+                    scriptbase.Main(objects);
+
+                }
+                else
+                {
+                    throw new ScriptException("IScriptEntry interface not found.");
+                }
+            }
+            else
+            {
+                throw new ScriptException("IScriptEntry interface not found.");
+            }
         }
     }
 }
