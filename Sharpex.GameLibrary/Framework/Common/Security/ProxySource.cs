63b70961d46c52fee0cb64fdfc06179a9af50267
ThuCommix <admin@thucommix.de>
2014-04-02 08:25:38 +0200

    Added sandboxed assembly handling

---
+++ Sharpex.GameLibrary/Framework/Common/Security/ProxySource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SharpexGL.Framework.Common.Security
+{
+    public abstract class ProxySource : MarshalByRefObject
+    {
+        /// <summary>
+        /// Gets the LifetimeService.
+        /// </summary>
+        /// <returns>ILease object.</returns>
+        public new virtual object GetLifetimeService()
+        {
+            return base.GetLifetimeService();
+        }
+    }
+}
