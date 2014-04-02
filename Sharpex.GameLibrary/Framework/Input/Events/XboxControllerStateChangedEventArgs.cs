d8141066281fa10706a889e71269db0b2dfccac6
ThuCommix <admin@thucommix.de>
2014-04-02 11:23:38 +0200

    Added support for XBOX360 Controller

---
+++ Sharpex.GameLibrary/Framework/Input/Events/XboxControllerStateChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using SharpexGL.Framework.Events;
+using SharpexGL.Framework.Input.DirectInput;
+
+namespace SharpexGL.Framework.Input.Events
+{
+    public class XboxControllerStateChangedEventArgs : IEvent
+    {
+        /// <summary>
+        /// Sets or gets the CurrentInputState.
+        /// </summary>
+        public XInputState CurrentInputState { get; set; }
+        /// <summary>
+        /// Sets or gets the PreviousInputState.
+        /// </summary>
+        public XInputState PreviousInputState { get; set; }
+    }
+}
