7aae965dbd043890b01f39e440de58f2b82125b5
ThuCommix <admin@thucommix.de>
2014-04-21 11:47:02 +0200

    Implement component structures

---
+++ Sharpex2D Java/framework/components/IComponent.java
@@ -0,0 +1,12 @@
+package sharpex2d.framework.components;
+
+import java.util.UUID;
+
+public interface IComponent {
+
+	/**
+	 * Gets the Guid.
+	 * @return The Guid.
+	 */
+	UUID getGuid();
+}
---
+++ Sharpex2D Java/framework/components/IConstructable.java
@@ -0,0 +1,9 @@
+package sharpex2d.framework.components;
+
+public interface IConstructable extends IComponent {
+
+	/**
+	 * Constructs the component.
+	 */
+	void construct();
+}
