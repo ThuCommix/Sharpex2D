8658c0d9f98585f714cc2ef14ac6414e2b078c6e
ThuCommix <admin@thucommix.de>
2014-05-21 14:40:05 +0200

    Added IContent implementation

--- Sharpex2D/Framework/Rendering/ITexture.cs
+++ Sharpex2D/Framework/Rendering/ITexture.cs
@@ -1,7 +1,8 @@
-﻿
+﻿using Sharpex2D.Framework.Content;
+
 namespace Sharpex2D.Framework.Rendering
 {
-    public interface ITexture
+    public interface ITexture : IContent
     {
         /// <summary>
         /// Gets the Width.
