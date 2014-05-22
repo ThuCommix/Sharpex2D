ce87240729657aeb01fd08fdb048dd37d6ca8004
ThuCommix <admin@thucommix.de>
2014-05-22 11:09:19 +0200

    Added AsyncAssetLoader
    The AsyncAssetLoader is designed to handle multiple asset loading in a
    new thread. The class dispatches the current progress state. Due the
    assets are not loading at game start like the OnContentLoad method, you
    can draw the game loading state. (Loading Screen)

---
+++ Sharpex2D/Framework/Content/AsyncAssetLoader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Sharpex2D.Framework.Content.Storage;
+
+namespace Sharpex2D.Framework.Content
+{
+    public class AsyncAssetLoader<T> where T : IContent
+    {
+
+        private readonly Dictionary<string, string> _queue;
+        private readonly ContentManager _contentManager;
+
+        /// <summary>
+        /// Initializes a new AssetLoader class.
+        /// </summary>
+        /// <param name="contentManager">The ContentManager.</param>
+        public AsyncAssetLoader(ContentManager contentManager)
+        {
+            ProgressPercentage = 0;
+            _contentManager = contentManager;
+            _queue = new Dictionary<string, string>();
+        }
+        /// <summary>
+        /// Gets the ProgressPercentage.
+        /// </summary>
+        public int ProgressPercentage { get; private set; }
+        /// <summary>
+        /// Sets or gets the Description.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Adds a new task to the queue.
+        /// </summary>
+        /// <param name="name">The later name of the ContentStorage item.</param>
+        /// <param name="assetpath">The AssetPath.</param>
+        public void Queque(string name, string assetpath)
+        {
+            if (_queue.ContainsKey(name))
+            {
+                throw new ArgumentException("The name already exists.");
+            }
+
+            _queue.Add(name, assetpath);
+        }
+        /// <summary>
+        /// Loads all quequed assets async.
+        /// </summary>
+        public void LoadAsync()
+        {
+            ProgressPercentage = 0;
+            var task = new Task(InternalLoadAsync);
+            task.Start();
+        }
+        /// <summary>
+        /// InternalLoadAsync.
+        /// </summary>
+        private void InternalLoadAsync()
+        {
+            var assetCount = _queue.Count;
+            var processedAssets = 0;
+
+            foreach (var asset in _queue)
+            {
+                ContentStorage.Add(asset.Key, _contentManager.Load<T>(asset.Value));
+                processedAssets++;
+                ProgressPercentage = 100*processedAssets/assetCount;
+            }
+
+            _queue.Clear();
+        }
+    }
+}
