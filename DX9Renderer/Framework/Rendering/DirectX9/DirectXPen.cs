1861cc856d035191642ecd15049669138d77f654
ThuCommix <admin@thucommix.de>
2014-05-28 15:19:26 +0200

    Added DirectXPen for 2d draw operations

---
+++ DX9Renderer/Framework/Rendering/DirectX9/DirectXPen.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Sharpex2D.Framework.Rendering.DirectX9
+{
+    public class DirectXPen : IPen
+    {
+        #region IPen Implementation
+        /// <summary>
+        /// Gets the Width.
+        /// </summary>
+        public float Width { get; set; }
+        /// <summary>
+        /// Gets the Color.
+        /// </summary>
+        public Color Color { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Initializes a new DirectXPen class.
+        /// </summary>
+        public DirectXPen()
+        {
+            
+        }
+        /// <summary>
+        /// Initializes a new DirectXPen class.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="width"></param>
+        public DirectXPen(Color color, float width)
+        {
+            Color = color;
+            Width = width;
+        }
+    }
+}
