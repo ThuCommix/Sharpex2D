412b58a8124adf878714ec65d77c88833c09cdf0
ThuCommix <admin@thucommix.de>
2014-06-04 10:08:15 +0200

    Implemented correct IDisposable pattern

--- Sharpex2D/Framework/Surface/RenderTarget.cs
+++ Sharpex2D/Framework/Surface/RenderTarget.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-using System.Runtime.InteropServices;
+using System.Diagnostics;
 using System.Threading;
 using System.Windows.Forms;
 using Sharpex2D.Framework.Components;
 
 namespace Sharpex2D.Framework.Surface
 {
+    [Developer("ThuCommix", "developer@sharpex2d.de")]
+    [Copyright("©Sharpex2D 2013 - 2014")]
+    [TestState(TestState.Tested)]
     public class RenderTarget : IComponent, IDisposable
     {
         #region IComponent Implementation
 
         /// <summary>
-        /// Sets or gets the Guid of the Component.
+        ///     Sets or gets the Guid of the Component.
         /// </summary>
         public Guid Guid
         {
@@ -20,32 +23,39 @@ namespace Sharpex2D.Framework.Surface
 
         #endregion
 
-        #region WinApi
-
-        /// <summary>
-        /// A value indicating whether the Window is valid.
-        /// </summary>
-        /// <param name="hWnd">The Handle.</param>
-        /// <returns>True if window is valid</returns>
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool IsWindow(IntPtr hWnd);
-
-        #endregion
+        private bool _isDisposed;
 
         #region IDisposable Implementation
+
         /// <summary>
-        /// Disposes the object.
+        ///     Disposes the object.
         /// </summary>
         public void Dispose()
         {
-            SurfaceControl.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Disposes the object.
+        /// </summary>
+        /// <param name="disposing">Indicates whether managed resources should be disposed.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                _isDisposed = true;
+                if (disposing)
+                {
+                    SurfaceControl.Dispose();
+                }
+            }
         }
 
         #endregion
 
         /// <summary>
-        /// Initializes a new RenderTarget class.
+        ///     Initializes a new RenderTarget class.
         /// </summary>
         /// <param name="handle">The WindowHandle.</param>
         internal RenderTarget(IntPtr handle)
@@ -55,74 +65,79 @@ namespace Sharpex2D.Framework.Surface
         }
 
         /// <summary>
-        /// Gets the WindowHandle.
+        ///     Gets the WindowHandle.
         /// </summary>
         public IntPtr Handle { get; private set; }
+
         /// <summary>
-        /// Gets the ISurfaceControl.
+        ///     Gets the ISurfaceControl.
         /// </summary>
         public ISurfaceControl SurfaceControl { private set; get; }
+
         /// <summary>
-        /// A value indicating whether the surface is running in fullscreen.
+        ///     A value indicating whether the surface is running in fullscreen.
         /// </summary>
         public bool IsFullscreen
         {
             get { return SurfaceControl.IsFullscreen(); }
         }
+
         /// <summary>
-        /// A value indicating whether the RenderTarget is valid.
+        ///     A value indicating whether the RenderTarget is valid.
         /// </summary>
         public bool IsValid
         {
-            get { return IsWindow(Handle); }
+            get { return NativeMethods.IsWindow(Handle); }
         }
+
         /// <summary>
-        /// Create a new RenderTarget from a specified handle.
+        ///     Create a new RenderTarget from a specified handle.
         /// </summary>
         /// <param name="handle">The Handle.</param>
         /// <returns>RenderTarget</returns>
         public static RenderTarget FromHandle(IntPtr handle)
         {
-            if (IsWindow(handle))
+            if (NativeMethods.IsWindow(handle))
             {
                 return new RenderTarget(handle);
             }
 
             throw new InvalidOperationException("The given Handle is not a window.");
         }
+
         /// <summary>
-        /// Gets the RenderTarget associated with the current process.
+        ///     Gets the RenderTarget associated with the current process.
         /// </summary>
         /// <returns>RenderTarget</returns>
         public static RenderTarget GetDefault()
         {
-            var handle = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
 
-            if (IsWindow(handle))
+            if (NativeMethods.IsWindow(handle))
             {
                 return new RenderTarget(handle);
             }
 
             throw new InvalidOperationException("Could not get the handle associated with the current process.");
         }
+
         /// <summary>
-        /// Creates a new RenderTarget.
+        ///     Creates a new RenderTarget.
         /// </summary>
         /// <returns>RenderTarget</returns>
         public static RenderTarget Create()
         {
             var surface = new Form();
-            
+
             new Thread(() => Application.Run(surface)).Start();
 
-            while (!surface.IsHandleCreated) { }
-
-            var handle = IntPtr.Zero;
-
-            MethodInvoker br = delegate
+            while (!surface.IsHandleCreated)
             {
-                handle = surface.Handle;
-            };
+            }
+
+            IntPtr handle = IntPtr.Zero;
+
+            MethodInvoker br = delegate { handle = surface.Handle; };
             surface.Invoke(br);
 
             return new RenderTarget(handle);
