b797c09a240bf41ca99e9287627eaa8065ff9a70
ThuCommix <admin@thucommix.de>
2014-06-04 10:02:21 +0200

    Renamed PluginLoader to PluginActivator

--- Sharpex2D/Framework/Plugin/PluginActivator.cs
+++ Sharpex2D/Framework/Plugin/PluginActivator.cs
@@ -6,35 +6,43 @@ using Sharpex2D.Framework.Components;
 
 namespace Sharpex2D.Framework.Plugin
 {
-    public class PluginLoader : IComponent
+    [Developer("ThuCommix", "developer@sharpex2d.de")]
+    [Copyright("©Sharpex2D 2013 - 2014")]
+    [TestState(TestState.Tested)]
+    public class PluginActivator : IComponent
     {
         #region IComponent Implementation
+
         /// <summary>
-        /// Gets the guid.
+        ///     Gets the guid.
         /// </summary>
-        public Guid Guid { get { return new Guid("520649BC-EAF2-48D4-9938-C240502E8681"); } }
+        public Guid Guid
+        {
+            get { return new Guid("520649BC-EAF2-48D4-9938-C240502E8681"); }
+        }
+
         #endregion
 
         /// <summary>
-        /// Loads a plugin into SGL.
+        ///     Loads a plugin into SGL.
         /// </summary>
         /// <typeparam name="T">The Type.</typeparam>
         /// <param name="path">The Path.</param>
         /// <returns>T</returns>
-        public static T Load<T>(string path)
+        public static T CreateInstance<T>(string path)
         {
             if (!File.Exists(path))
             {
                 throw new FileNotFoundException("The given resource could not be located.");
             }
-            var assembly = Assembly.LoadFrom(path);
+            Assembly assembly = Assembly.LoadFrom(path);
 
             if (assembly.GetTypes().Any(type => type == typeof (T)))
             {
-                return (T)((object)assembly);
+                return (T) ((object) assembly);
             }
 
-            throw new PluginException("The resource is not a valid " + typeof(T).FullName + ".");
+            throw new PluginException("The resource is not a valid " + typeof (T).FullName + ".");
         }
     }
 }
