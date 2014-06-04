b61f7f8b87fc2ebc0f02b8e9d62c46b6a1927286
ThuCommix <admin@thucommix.de>
2014-06-04 09:36:54 +0200

    Determines if classes are tested

---
+++ Sharpex2D/Development/TestState.cs
@@ -0,0 +1,20 @@
+﻿namespace Sharpex2D
+{
+    public enum TestState
+    {
+        /// <summary>
+        ///     The component is not tested.
+        /// </summary>
+        Untested = 0,
+
+        /// <summary>
+        ///     The component is currently under testing.
+        /// </summary>
+        InProgress = 1,
+
+        /// <summary>
+        ///     The component is tested.
+        /// </summary>
+        Tested = 2
+    }
+}
