80581f25c5ec7a28a779c136cee967f5a5e49c87
ThuCommix <admin@thucommix.de>
2014-06-04 10:07:06 +0200

    Moved P/Invoke to NativeMethods

---
+++ Sharpex2D/Framework/Surface/NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sharpex2D.Framework.Surface
+{
+    public class NativeMethods
+    {
+        /// <summary>
+        ///     A value indicating whether the Window is valid.
+        /// </summary>
+        /// <param name="hWnd">The Handle.</param>
+        /// <returns>True if window is valid</returns>
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool IsWindow(IntPtr hWnd);
+    }
+}
--- Sharpex2D/Framework/Surface/WindowController.cs
+++ Sharpex2D/Framework/Surface/WindowController.cs
@@ -1,9 +1,13 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using Sharpex2D.Framework.Math;
 
 namespace Sharpex2D.Framework.Surface
 {
+    [Developer("ThuCommix", "developer@sharpex2d.de")]
+    [Copyright("©Sharpex2D 2013 - 2014")]
+    [TestState(TestState.Tested)]
     public class WindowController : ISurfaceControl
     {
         #region ISurfaceControl Implementation
@@ -11,63 +15,51 @@ namespace Sharpex2D.Framework.Surface
         private bool _isFullscreen;
 
         /// <summary>
-        /// Sets the Title of the GameWindow.
+        ///     Sets the Title of the GameWindow.
         /// </summary>
         /// <param name="value">The Value.</param>
         public void SetTitle(string value)
         {
-            MethodInvoker br = delegate
-            {
-                _surface.Text = value;
-            };
+            MethodInvoker br = delegate { _surface.Text = value; };
             _surface.Invoke(br);
         }
 
         /// <summary>
-        /// Sets the Icon of the GameWindow.
+        ///     Sets the Icon of the GameWindow.
         /// </summary>
         /// <param name="icon">The Icon.</param>
         public void SetIcon(Icon icon)
         {
-            MethodInvoker br = delegate
-            {
-                _surface.Icon = icon;
-            };
+            MethodInvoker br = delegate { _surface.Icon = icon; };
             _surface.Invoke(br);
         }
 
         /// <summary>
-        /// Sets the Size of the GameWindow.
+        ///     Sets the Size of the GameWindow.
         /// </summary>
         /// <param name="width">The Width.</param>
         /// <param name="height">The Height.</param>
         public void SetSize(int width, int height)
         {
             FreeWindow();
-            MethodInvoker br = delegate
-            {
-                _surface.ClientSize = new Size(width, height);
-            };
+            MethodInvoker br = delegate { _surface.ClientSize = new Size(width, height); };
             _surface.Invoke(br);
             FixWindow();
         }
 
         /// <summary>
-        /// Sets the Position of the GameWindow.
+        ///     Sets the Position of the GameWindow.
         /// </summary>
         /// <param name="x">The XKoord.</param>
         /// <param name="y">The YKoord.</param>
         public void SetPosition(int x, int y)
         {
-            MethodInvoker br = delegate
-            {
-                _surface.Location = new Point(x, y);
-            };
+            MethodInvoker br = delegate { _surface.Location = new Point(x, y); };
             _surface.Invoke(br);
         }
 
         /// <summary>
-        /// Sets the Style of the GameWindow.
+        ///     Sets the Style of the GameWindow.
         /// </summary>
         /// <param name="style">The WindowStyle.</param>
         public void SetWindowStyle(SurfaceStyle style)
@@ -87,8 +79,8 @@ namespace Sharpex2D.Framework.Surface
                     _surface.FormBorderStyle = FormBorderStyle.Sizable;
                     _surface.ClientSize = new Size(SGL.GraphicsDevice.DisplayMode.Width,
                         SGL.GraphicsDevice.DisplayMode.Height);
-                    _surface.Location = new Point((Screen.PrimaryScreen.WorkingArea.Width - _surface.Width) / 2,
-                          (Screen.PrimaryScreen.WorkingArea.Height - _surface.Height) / 2);
+                    _surface.Location = new Point((Screen.PrimaryScreen.WorkingArea.Width - _surface.Width)/2,
+                        (Screen.PrimaryScreen.WorkingArea.Height - _surface.Height)/2);
                     _isFullscreen = false;
                 }
             };
@@ -97,22 +89,19 @@ namespace Sharpex2D.Framework.Surface
         }
 
         /// <summary>
-        /// Gets the Soue of the GameWindow.
+        ///     Gets the Soue of the GameWindow.
         /// </summary>
         /// <returns>Size</returns>
         public Vector2 GetSize()
         {
             Vector2 size = null;
-            MethodInvoker br = delegate
-            {
-                size = new Vector2(_surface.Location.X, _surface.Location.Y);
-            };
+            MethodInvoker br = delegate { size = new Vector2(_surface.Location.X, _surface.Location.Y); };
             _surface.Invoke(br);
             return size;
         }
 
         /// <summary>
-        /// Sets the ControlLayout.
+        ///     Sets the ControlLayout.
         /// </summary>
         /// <param name="surfaceLayout">The SurfaceLayout.</param>
         public void SetControlLayout(SurfaceLayout surfaceLayout)
@@ -127,7 +116,7 @@ namespace Sharpex2D.Framework.Surface
         }
 
         /// <summary>
-        /// Sets the Cursor visibility.
+        ///     Sets the Cursor visibility.
         /// </summary>
         /// <param name="state">The State.</param>
         public void SetCursorVisibility(bool state)
@@ -141,35 +130,67 @@ namespace Sharpex2D.Framework.Surface
                 Cursor.Hide();
             }
         }
+
         /// <summary>
-        /// Sets the Cursor icon.
+        ///     Sets the Cursor icon.
         /// </summary>
         /// <param name="iconPath">The IconPath.</param>
         public void SetCursorIcon(string iconPath)
         {
-            MethodInvoker br = delegate
-            {
-                Cursor.Current = new Cursor(iconPath);
-            };
+            MethodInvoker br = delegate { Cursor.Current = new Cursor(iconPath); };
             _surface.Invoke(br);
+        }
+
+        /// <summary>
+        ///     Indicating whether the surface is active.
+        /// </summary>
+        /// <returns>True if active.</returns>
+        public bool IsActive()
+        {
+            bool flag = true;
+            MethodInvoker br = delegate { flag = _surface.Focused; };
+            _surface.Invoke(br);
+
+            return flag;
         }
 
         #endregion
 
+        private readonly Form _surface;
+        private bool _isDisposed;
+
         #region IDisposable Implementation
+
         /// <summary>
-        /// Disposes the object.
+        ///     Disposes the object.
         /// </summary>
         public void Dispose()
         {
-            MethodInvoker br = () => _surface.Close();
-            _surface.Invoke(br);
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Disposes the object.
+        /// </summary>
+        /// <param name="disposing">Indicates whether managed resources should be disposed.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                _isDisposed = true;
+                if (disposing)
+                {
+                    MethodInvoker br = () => _surface.Dispose();
+                    _surface.Invoke(br);
+                }
+            }
         }
 
         #endregion
 
         /// <summary>
-        /// Initializes a new WindowContoller class.
+        ///     Initializes a new WindowContoller class.
         /// </summary>
         /// <param name="renderTarget">The RenderTarget.</param>
         public WindowController(RenderTarget renderTarget)
@@ -178,21 +199,53 @@ namespace Sharpex2D.Framework.Surface
             SetCursorVisibility(false);
             SetControlLayout(new SurfaceLayout(true, false, true));
             _surface.FormClosing += _surface_FormClosing;
+            _surface.Activated += _surface_Activated;
+            _surface.Deactivate += _surface_Deactivate;
         }
 
         /// <summary>
-        /// Called, if the surface is closing.
+        ///     Indicating whether the surface is running in fullscreen.
+        /// </summary>
+        /// <returns>True if fullscreen is activated</returns>
+        public bool IsFullscreen()
+        {
+            return _isFullscreen;
+        }
+
+        /// <summary>
+        ///     Called, if the surface is closing.
         /// </summary>
         /// <param name="sender">The Sender.</param>
         /// <param name="e">The EventArgs.</param>
-        void _surface_FormClosing(object sender, FormClosingEventArgs e)
+        private void _surface_FormClosing(object sender, FormClosingEventArgs e)
         {
             e.Cancel = true;
             SGL.Shutdown();
         }
 
         /// <summary>
-        /// Fixes the size of the window.
+        ///     Called, if the surface is deactivated.
+        /// </summary>
+        /// <param name="sender">The Sender.</param>
+        /// <param name="e">The EventArgs.</param>
+        private void _surface_Deactivate(object sender, EventArgs e)
+        {
+            SGL.GameInstance.OnDeactivated();
+        }
+
+        /// <summary>
+        ///     Called, if the surface is activated.
+        /// </summary>
+        /// <param name="sender">The Sender.</param>
+        /// <param name="e">The EventArgs.</param>
+        private void _surface_Activated(object sender, EventArgs e)
+        {
+            SGL.GameInstance.OnActivated();
+        }
+
+
+        /// <summary>
+        ///     Fixes the size of the window.
         /// </summary>
         private void FixWindow()
         {
@@ -203,8 +256,9 @@ namespace Sharpex2D.Framework.Surface
             };
             _surface.Invoke(br);
         }
+
         /// <summary>
-        /// Frees the window size.
+        ///     Frees the window size.
         /// </summary>
         private void FreeWindow()
         {
@@ -215,15 +269,5 @@ namespace Sharpex2D.Framework.Surface
             };
             _surface.Invoke(br);
         }
-        /// <summary>
-        /// Indicating whether the surface is running in fullscreen.
-        /// </summary>
-        /// <returns>True if fullscreen is activated</returns>
-        public bool IsFullscreen()
-        {
-            return _isFullscreen;
-        }
-
-        private readonly Form _surface;
     }
 }
