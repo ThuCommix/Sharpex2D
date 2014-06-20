71714cda6139da296a6f9a37ffd485df2f5be4ba
ThuCommix <admin@thucommix.de>
2014-06-20 17:16:04 +0200

    Added Font and Pen, can be used by any RenderDevice

---
+++ Sharpex2D/Framework/Rendering/Fonts/Font.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Sharpex2D.Framework.Content.Pipeline;
+using Sharpex2D.Framework.Rendering.Devices;
+
+namespace Sharpex2D.Framework.Rendering.Fonts
+{
+    [Developer("ThuCommix", "developer@sharpex2d.de")]
+    [Copyright("©Sharpex2D 2013 - 2014")]
+    [TestState(TestState.Tested)]
+    [Content("Font")]
+    public class Font : IDeviceResource
+    {
+        #region IRendererResource Implementation
+
+        /// <summary>
+        ///     Gets or sets the DebugName.
+        /// </summary>
+        public string DebugName { get; set; }
+
+        #endregion
+
+        /// <summary>
+        ///     Initializes a new Typeface.
+        /// </summary>
+        /// <param name="typeface">The Typeface.</param>
+        public Font(Typeface typeface)
+        {
+            RenderDevice rendererInstance = SGL.RenderDevice;
+            Instance = rendererInstance.ResourceManager.CreateResource(typeface);
+        }
+
+        /// <summary>
+        ///     Gets the Instance.
+        /// </summary>
+        public IFont Instance { private set; get; }
+
+        /// <summary>
+        ///     Gets the Type.
+        /// </summary>
+        public Type Type { private set; get; }
+
+        /// <summary>
+        ///     Gets the Typeface.
+        /// </summary>
+        public Typeface Typeface
+        {
+            get { return Instance.Typeface; }
+        }
+    }
+}
