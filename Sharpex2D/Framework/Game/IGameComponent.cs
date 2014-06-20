75405d1f84baacae420395615eb3cee758b25132
ThuCommix <admin@thucommix.de>
2014-06-20 17:19:04 +0200

    Added GameComponent collection to Game class

---
+++ Sharpex2D/Framework/Game/IGameComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace Sharpex2D.Framework.Game
+{
+    [Developer("ThuCommix", "developer@sharpex2d.de")]
+    [Copyright("©Sharpex2D 2013 - 2014")]
+    [TestState(TestState.Tested)]
+    public interface IGameComponent : IUpdateable, IDrawable
+    {
+        /// <summary>
+        ///     Gets the Order.
+        /// </summary>
+        int Order { get; }
+    }
+}
